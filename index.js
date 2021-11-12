728725b3c02093c38fcc1dd6fdd9ef034b1da03c
Your Namedkrmentor <Your Namedkrmentor@gmail.com>
2021-11-12 19:11:58 +0500

    practice

---
+++ index.js
@@ -0,0 +1,131 @@
+/* multi line comment */
+
+// 1.WAYS TO PRINT IN JS
+// console.log("Hello World")
+// alert("me")
+// document.write("This is document write")
+
+// 2.CONSOLE API
+// console.warn("warning")
+// console.error("error")
+// console.clear()
+// console.assert(4==6)
+// console.assert(4==(6-2))
+// console.log("add 2+4 =",2+4, "adition done")
+
+//3.JS Variable
+// variable: containers to store data values
+// var num = 34;
+// var num2 = 56;
+// console.log(num+num2);
+
+//4.Data Types
+
+/*
+        at  a very high level , there are two type of data type in JS
+        1.primitive:undefined, null, numbers, strings, boolean, symbol
+        2.reference:arrays, objects
+
+*/
+//num
+// var num1 =455;
+// var num2 = 56.76;
+// console.log(num1 , num2);
+// string
+// var str1 = "This is a string"
+// var str2 = 'This is a string'
+// console.log(str1 , str2);
+// objects
+// var marks={
+//     dhara:99.977,
+//     ravi:78,
+//     shubham:34,
+// }
+// console.log(marks);
+//boolean
+// var a = "true";
+// var b ="false";
+// console.log(a,b);
+//undefined
+// var und;
+// console.log(und);
+//null
+// var n = null;
+// console.log(n);
+// var arr = [1,"dhara","string sdasdas",4,5]
+// console.log(arr)
+//count start from 0 in array
+// console.log(arr[4])
+// console.log(arr[1])
+
+//5.Arthemetic Operators (+ - / *)
+// var a =34;
+// var b =56;
+// console.log("the value of a + b is :",a+b);
+// console.log("the value of a + b is :",a-b);
+// //or you can do it like this
+// console.log("the value of a + b is : "+(a+b));
+// console.log("the value of a + b is : "+(a-b));
+
+//6.Assignment Operators ()
+// var b=56;
+// var c=b;
+// console.log(c);   
+// console.log(c+=2);
+// console.log(c-=2);
+// console.log(c*=2);
+// console.log(c/=2);
+
+//7.Comparision Operators ()
+// var x =43;
+// var y =56;
+// console.log(x==y)
+// console.log(x>=y)
+// console.log(x<=y)
+// console.log(x>y)
+// console.log(x<y)
+
+//8.Logical Operators () 
+// and
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+// or
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+// not
+// console.log(!true);
+// console.log(false);
+
+//8.functions() 
+// function avg(a,b){
+//    c=(a+b)/2
+//    return c;
+// }
+// c1=avg(4,6);
+// c2=avg(14,16);
+// console.log(c1,c2); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
